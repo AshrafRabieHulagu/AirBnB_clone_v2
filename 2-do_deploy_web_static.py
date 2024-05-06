f60eab48e3515eab56883cd06c4568618db83f30
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-05-06 04:05:07 -0700

    AirBnB_clone_v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,27 +1,47 @@
 #!/usr/bin/python3
-''' a fabric script that distributes an archive to a server '''
-from fabric.api import put, sudo, env, run
-from os.path import exists
-env.hosts = ['ubuntu@54.85.93.141', 'ubuntu@34.201.165.179']
+'''Module distributes an archive to your web servers
+using the function do_deploy'''
+
+from datetime import datetime
+from fabric.api import local, env, put, sudo
+import os
+
+
+env.user = 'ubuntu'
+env.hosts = ['34.75.36.193', '3.94.194.152']
 
 
 def do_deploy(archive_path):
-    ''' function that uploads to server and uncompress the archive file '''
-    if not exists(archive_path):
+    '''Method to deploy archive'''
+
+    if os.path.exists(archive_path) is False:
         return False
 
+    arch = archive_path.split("/", 2)[1]
+    new_file = '/data/web_static/releases/{}'.format(
+        arch.split(".", 1)[0])
+    tmp_file = '/tmp/{}'.format(arch)
+    cur = '/data/web_static/current'
+
+    '''
+    archive_path = versions/web_static_20210415160740.tgz
+    arch = web_static_20210415160740.tgz
+    new_file = /data/web_static/releases/web_static_20210415160740
+    tmp_file = /tmp/web_static_20210415160740.tgz
+    cur = /data/web_static/current
+    '''
+
     try:
-        filename = archive_path.rsplit('/', 1)[-1]
-        filenamextn = filename.rsplit('.', 1)[0]
-        filepath = '/data/web_static/releases/{}/'.format(filenamextn)
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(filepath))
-        run("tar -xzf /tmp/{} -C {}".format(filename, filepath))
-        run("rm /tmp/{}".format(filename))
-        run("mv {}web_static/* {}".format(filepath, filepath))
-        run("rm -rf {}web_static".format(filepath))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(filepath))
-        return True
+        put(archive_path, '/tmp/')
+        sudo("mkdir -p {}/".format(new_file))
+        sudo("tar -xzf {} -C {}/".format(tmp_file, new_file))
+        sudo("rm {}".format(tmp_file))
+        sudo("mv {}/web_static/* {}".format(new_file, new_file))
+        sudo("rm -rf {}/web_static".format(new_file))
+        sudo("rm -rf {}".format(cur))
+        sudo("ln -s {}/ {}".format(new_file, cur))
+        print("YAY! LOVE YOU!")
+        return(True)
     except:
-        return False
+        print("KEEP TRYING! :)")
+        return(False)
