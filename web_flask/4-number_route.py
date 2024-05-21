0f73a2e4bfb3bea7c1d157916095dfc21dfa4ae1
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-05-21 05:54:01 -0700

    web_flask

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,43 +1,46 @@
 #!/usr/bin/python3
-"""This file is setting up a flask web app"""
-from flask import Flask, escape, request
+"""Start web application with two routings
+"""
 
-
+from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello_hbnb():
-    """Prints Hello HBNB"""
-    return "Hello HBNB!"
+def hello():
+    """Return string when route queried
+    """
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """route hbnb: prints hbnb"""
-    return "HBNB"
+    """Return string when route queried
+    """
+    return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c(text):
-    """prints c is followed by text"""
-    nt = text.replace('_', ' ')
-    return "C {}".format(nt)
+def c_is_fun(text):
+    """Return reformatted text
+    """
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/')
 @app.route('/python/<text>')
-def pyis(text='is cool'):
-    """prints python followed by text, with default"""
-    nt = text.replace('_', ' ')
-    return "Python {}".format(nt)
+def python_with_text(text='is cool'):
+    """Reformat text based on optional variable
+    """
+    return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>')
-def num(n):
-    """some random text here, brain.exe failed to compile"""
-    return "{} is a number".format(n)
+def number(n=None):
+    """Allow request if path variable is a valid integer
+    """
+    return str(n) + ' is a number'
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
