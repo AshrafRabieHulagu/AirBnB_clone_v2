7c627b93826c7a6e9b0db6a9a7f61fbae5035d15
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-05-22 04:37:21 -0700

    0x04. AirBnB clone - Web framework

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,11 +1,23 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
 
 
 @app.route("/states", strict_slashes=False)
 def states():
-    """
-    Displays HTML page with list of states
-    States sorted by name
+    """Displays an HTML page with a list of all States.
+
+    States are sorted by name.
     """
     states = storage.all("State")
     return render_template("9-states.html", state=states)
@@ -13,9 +25,7 @@ def states():
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
-    """
-    Displays HTML page with infor on <id> if it exists
-    """
+    """Displays an HTML page with info about <id>, if it exists."""
     for state in storage.all("State").values():
         if state.id == id:
             return render_template("9-states.html", state=state)
@@ -23,8 +33,8 @@ def states_id(id):
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """Remove current SQLAlchemy session"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
