79bedf2242fbdc25a75707f26323ec42f6b4a917
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-05-22 01:25:52 -0700

    0x04. AirBnB clone - Web framework

--- models/state.py
+++ models/state.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
-from os import getenv
-from models.city import City
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column
-from sqlalchemy import String
+from sqlalchemy import Column, Integer, String
+from os import getenv
 
 
 class State(BaseModel, Base):
-    """
-    Represents state for MySQL database
-    Inherits from SPLAlchemy Base and connects to MySQL table states
-    
-    Attributes:
-        __tablename__ (str): Name of MySQL table to store states
-        name (sqlalchemy String): name of state
-        cities (sqlalchemy relationship): state city connection
-    """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="states")
-    
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    """ State class """
+    __tablename__ = 'states'
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='delete')
+    else:
+        name = ""
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         @property
         def cities(self):
-            """Get list of related city objects"""
-            city_list = []
-            for city in list(models.storage.all(City).values()):
+            cities_list = []
+            all_cities = models.storage.all(City).values()
+            for city in all_cities:
                 if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+                    cities_list.append(city)
+            return cities_list
