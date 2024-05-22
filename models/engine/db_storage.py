9c032998721eeafc3929d6e98c98a618bf0bb6ab
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-05-22 03:26:00 -0700

    0x04. AirBnB clone - Web framework

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" new storage engine sqlAlchemy """
+""" new class for sqlAlchemy """
 from os import getenv
-from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
 from models.state import State
 from models.city import City
@@ -9,7 +11,6 @@ from models.user import User
 from models.place import Place
 from models.review import Review
 from models.amenity import Amenity
-from sqlalchemy.orm import sessionmaker, scoped_session
 
 
 class DBStorage:
@@ -20,8 +21,8 @@ class DBStorage:
     def __init__(self):
         user = getenv("HBNB_MYSQL_USER")
         passwd = getenv("HBNB_MYSQL_PWD")
-        host = getenv("HBNB_MYSQL_HOST")
         db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
         env = getenv("HBNB_ENV")
 
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
@@ -36,33 +37,36 @@ class DBStorage:
         Return:
             returns a dictionary of __object
         """
-        dico = {}
+        dic = {}
         if cls:
             if type(cls) is str:
                 cls = eval(cls)
             query = self.__session.query(cls)
             for elem in query:
                 key = "{}.{}".format(type(elem).__name__, elem.id)
-                dico[key] = elem
+                dic[key] = elem
         else:
-            tables = [State, City, User, Place, Review, Amenity]
-            for table in tables:
-                query = self.__session.query(table)
+            lista = [State, City, User, Place, Review, Amenity]
+            for clase in lista:
+                query = self.__session.query(clase)
                 for elem in query:
                     key = "{}.{}".format(type(elem).__name__, elem.id)
-                    dico[key] = elem
-        return (dico)
+                    dic[key] = elem
+        return (dic)
 
     def new(self, obj):
-        """ add a new element in the table """
+        """add a new element in the table
+        """
         self.__session.add(obj)
 
     def save(self):
-        """ save changes """
+        """save changes
+        """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """ delete an element in the table """
+        """delete an element in the table
+        """
         if obj:
             self.session.delete(obj)
 
@@ -73,3 +77,8 @@ class DBStorage:
         sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sec)
         self.__session = Session()
+
+    def close(self):
+        """ calls remove()
+        """
+        self.__session.close()
