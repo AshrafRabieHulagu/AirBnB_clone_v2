32f44307d32caf6d809b0bc5d6bac7867de73856
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-05-22 01:23:42 -0700

    0x04. AirBnB clone - Web framework

--- models/state.py
+++ models/state.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 from os import getenv
+from models.city import City
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column
+from sqlalchemy import String
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
+    """
+    Represents state for MySQL database
+    Inherits from SPLAlchemy Base and connects to MySQL table states
+    
+    Attributes:
+        __tablename__ (str): Name of MySQL table to store states
+        name (sqlalchemy String): name of state
+        cities (sqlalchemy relationship): state city connection
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", backref="states")
+    
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """relationship between city and state"""
-            from models import storage
-            new_city = []
-            all_cities = storage.all(City)
-            for city in all_cities.values():
+            """Get list of related city objects"""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
                 if city.state_id == self.id:
-                    new_city.append(city)
-            return new_city
+                    city_list.append(city)
+            return city_list
