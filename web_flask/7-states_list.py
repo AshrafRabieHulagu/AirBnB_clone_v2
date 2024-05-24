68cb841392010523c465df5fab69e4a3bf740abb
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-05-24 09:23:47 -0700

    0x04. AirBnB clone - Web framework

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""Flask framework"""
+""" HBNB States with flask """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ teardown db"""
-    if storage is not None:
-        storage.close()
+def teardown(exception):
+    """ Tears down session """
+    storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
-def states_list():
-    """ list of state ids"""
-    data = storage.all(State)
-    return render_template('7-states_list.html', total=data.values())
+def display_states():
+    """ Render all the states in storage """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
