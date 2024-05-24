bdace1a65ec1c35b926fe75cce479c99d148815f
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-05-24 06:43:01 -0700

    0x04. AirBnB clone - Web framework

--- models/state.py
+++ models/state.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-import models
+"""This is the state class"""
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from os import getenv
+from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
+from sqlalchemy.orm import relationship, backref
+import models
+from os import environ
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state",
-                              cascade="all, delete-orphan")
+
+    if environ.get('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
+
         @property
         def cities(self):
-            """ Lists Of Instances Of Cities """
-            cityList = []
-            allCities = models.storage.all(City)
-            for city in allCities.values():
-                if city.state_id == self.id:
-                    cityList.append(city)
-            return cityList
+            """ Returns the list of City instances with
+            state_id == current State.id """
+            all_cities = models.storage.all(City)
+            state_cities = []
+            for city_ins in all_cities.values():
+                if city_ins.state_id == self.id:
+                    state_cities.append(city_ins)
+
+            return state_cities
