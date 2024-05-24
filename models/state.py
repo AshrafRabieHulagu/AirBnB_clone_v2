f2b1379e14aabb91975cb12f65651b6e529b6e80
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-05-24 08:17:21 -0700

    0x04. AirBnB clone - Web framework

--- models/state.py
+++ models/state.py
@@ -1,44 +1,25 @@
 #!/usr/bin/python3
-"""Module to define the State class
-"""
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models import storage
-from models.city import City
+import os
 
 
 class State(BaseModel, Base):
-    """Class that defines a State
-
-    Attributes:
-        name (str): the state name
-    """
+    """ State class """
     __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete, delete-orphan")
 
-    name = Column("name", String(128), nullable=False)
-    cities = relationship("City", backref="state")
-
-    def __init__(self, *args, **kwargs):
-        """Initializes the instance
-
-        Args:
-            args: positional arguments
-            kwargs: named arguments
-        """
-        dir_dct = self.__dir__()
-        for key in kwargs:
-            if key in dir_dct:
-                self.__dict__[key] = kwargs[key]
-        super().__init__(**kwargs)
-
-    @property
-    def cities(self):
-        """gets the cities for the file storage
-        """
-        all_cities = storage.all(City)
-        city_objs = all_cities.values()
-        self.__cities = [
-                city for city in city_objs if city.state_id == self.id
-                ]
-        return self.__cities
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            c = []
+            from models import storage
+            from models.city import City
+            for v in storage.all(City).values():
+                if v.state_id == self.id:
+                    c.append(v)
+            return c
