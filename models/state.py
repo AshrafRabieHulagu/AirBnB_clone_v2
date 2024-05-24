0b76b77e20b50b75d95dd9b02e9eb1d5e7d88b87
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-05-24 09:23:40 -0700

    0x04. AirBnB clone - Web framework

--- models/state.py
+++ models/state.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
-"""
-    contains state class to represent a state
-"""
-
-from models.base_model import BaseModel, Base
+""" State Module for HBNB project """
 import models
+from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import environ
-
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
+from os import getenv
 
 
 class State(BaseModel, Base):
-    """ State class: class to represent states of cities"""
-    if (storage_engine == 'db'):
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
+    """ State class """
+    __tablename__ = "states"
 
+    name = Column(String(128), nullable=False)
+    # cascade "all, delete" when a `state`` object is marked for
+    # deletion,its related `city` objects should also be marked for deletion
+    if getenv("HBNB_TYPE_STORAGE") == "db":  # is DB is in use?
+        cities = relationship('City', backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:  # use a property for file storage
         @property
         def cities(self):
-            """cities list
-            """
-            result = []
-            for j, i in models.storage.all(models.city.City).items():
-                if (i.state_id == self.id):
-                    result.append(i)
-            return result
+            """ cities implementation for `file` storage type """
+            all_cities = models.storage.all(City)
+            _cities = []
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    _cities.append(city)
+            return _cities
