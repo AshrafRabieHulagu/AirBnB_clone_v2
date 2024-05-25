0b98848b774f47498cbd84cf0887635b96ad35f5
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-05-25 05:53:38 -0700

    0x04. AirBnB clone - Web framework

--- models/state.py
+++ models/state.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-"""This is the state class"""
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
-import models
 from models.city import City
 from os import getenv
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
+    """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="delete", backref="state")
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
+        name = ""
+
         @property
         def cities(self):
-            """Returns a list of all city related objects"""
-            var = list(models.storage.all(City).values())
-            result = []
-            for city in var:
-                if (city.state_id == self.id):
-                    result.append(city)
-            return (result)
+            """Get all cities in the current state"""
+            from models import storage
+            cities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities.append(city)
+            return cities
