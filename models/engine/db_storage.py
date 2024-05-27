37159c44c6be06671b253e6c63698d9c1eaf0643
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-05-27 03:40:53 -0700

    0x04. AirBnB clone - Web framework

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,40 +1,84 @@
-# models/engine/db_storage.py
-from sqlalchemy import create_engine
+#!/usr/bin/python3
+""" new class for sqlAlchemy """
+from os import getenv
 from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
-import os
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+
 
 class DBStorage:
+    """ create tables in environmental"""
     __engine = None
     __session = None
 
     def __init__(self):
-        user = os.environ.get('HBNB_MYSQL_USER')
-        pwd = os.environ.get('HBNB_MYSQL_PWD')
-        host = os.environ.get('HBNB_MYSQL_HOST', default='localhost')
-        db = os.environ.get('HBNB_MYSQL_DB')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                      format(user, pwd, host, db),
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
                                       pool_pre_ping=True)
-        if os.environ.get('HBNB_ENV') == 'test':
+
+        if env == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        # TODO: Implement the all method
-        pass
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                dic[key] = elem
+        else:
+            lista = [State, City, User, Place, Review, Amenity]
+            for clase in lista:
+                query = self.__session.query(clase)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__, elem.id)
+                    dic[key] = elem
+        return (dic)
 
     def new(self, obj):
-        # TODO: Implement the new method
-        pass
+        """add a new element in the table
+        """
+        self.__session.add(obj)
 
     def save(self):
-        # TODO: Implement the save method
-        pass
+        """save changes
+        """
+        self.__session.commit()
 
     def delete(self, obj=None):
-        # TODO: Implement the delete method
-        pass
+        """delete an element in the table
+        """
+        if obj:
+            self.session.delete(obj)
 
     def reload(self):
-        # TODO: Implement the reload method
-        pass
+        """configuration
+        """
+        Base.metadata.create_all(self.__engine)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
+        self.__session = Session()
+
+    def close(self):
+        """ calls remove()
+        """
+        self.__session.remove()
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Script to start a Flask web application"""
-
+"""Script that starts a Flask server."""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,18 +7,24 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Display a list of all State objects sorted by name (A->Z)"""
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
-
-
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Remove the current SQLAlchemy Session"""
+    """
+    Method to remove the current SQLAlchemy Session after each request.
+    """
     storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    Route to display a HTML page with the list of all
+    State objects present in DBStorage.
+    """
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 if __name__ == '__main__':
