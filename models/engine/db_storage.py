a3fd16cab3d502df91431fba9e4f93e9964dbde6
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-05-27 00:19:01 -0700

    0x04. AirBnB clone - Web framework

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,105 +1,84 @@
 #!/usr/bin/python3
-"""This is the DB storage class for AirBnB"""
-
-from models.base_model import Base, BaseModel
-from models.user import User
+""" new class for sqlAlchemy """
+from os import getenv
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import Base
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
+from models.user import User
 from models.place import Place
 from models.review import Review
-import sys
-from os import getenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from models.amenity import Amenity
 
 
 class DBStorage:
-    """This class saves data to a MySQL database"""
-
+    """ create tables in environmental"""
     __engine = None
     __session = None
-    __filtered = {}
-    __all = {}
 
     def __init__(self):
-        """Instantiation of DBStorage class"""
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
 
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(getenv('HBNB_MYSQL_USER'),
-                                              getenv('HBNB_MYSQL_PWD'),
-                                              getenv('HBNB_MYSQL_HOST'),
-                                              getenv('HBNB_MYSQL_DB')),
+                                      .format(user, passwd, host, db),
                                       pool_pre_ping=True)
 
-        if getenv('HBNB_ENV') == 'test':
+        if env == "test":
             Base.metadata.drop_all(self.__engine)
-        else:
-            Session = sessionmaker(bind=self.__engine)
-            self.__session = Session()
 
     def all(self, cls=None):
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
         """
-        This returns all or one specific class object based on user's input as
-        a dictionary
-        """
-        classes = [User, State, City, Amenity, Place, Review]
-
-        if cls is None:
-
-            for cls in classes:
-
-                all_recs = self.__session.query(cls).all()
-
-                for i in all_recs:
-
-                    key = str(i.__class__.__name__) + "." + str(i.id)
-
-                    self.__all[key] = i
-
-            return self.__all
-
+        dic = {}
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                dic[key] = elem
         else:
-            if (isinstance(cls, str)):
-                fil_recs = self.__session.query(eval(cls)).all()
-
-            else:
-                fil_recs = self.__session.query(cls).all()
-
-            for value in fil_recs:
-
-                key = str(value.__class__.__name__) + "." + str(value.id)
-
-                self.__filtered[key] = value
-
-            return self.__filtered
+            lista = [State, City, User, Place, Review, Amenity]
+            for clase in lista:
+                query = self.__session.query(clase)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__, elem.id)
+                    dic[key] = elem
+        return (dic)
 
     def new(self, obj):
-        """Adds given object to DB session"""
+        """add a new element in the table
+        """
         self.__session.add(obj)
 
     def save(self):
-        """saves the current session to the MySQL database"""
+        """save changes
+        """
         self.__session.commit()
 
-    def reload(self):
-        """reload the database
-        """
-        try:
-            Base.metadata.create_all(self.__engine)
-            session_factory = sessionmaker(bind=self.__engine,
-                                           expire_on_commit=False)
-            Session = scoped_session(sessiobn_factory)
-            self.__session = Session
-        except:
-            pass
-
     def delete(self, obj=None):
-        """Delete obj if it’s inside"""
-        if obj is not None:
-            self.__session.delete(obj)
-            self.save()
+        """delete an element in the table
+        """
+        if obj:
+            self.session.delete(obj)
+
+    def reload(self):
+        """configuration
+        """
+        Base.metadata.create_all(self.__engine)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
+        self.__session = Session()
 
     def close(self):
-        """Removes session when needed"""
+        """ calls remove()
+        """
         self.__session.close()
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-
-
-"""utilizing Flask for Web app frame work"""
-
+'''A simple Flask web application.
+'''
 from flask import Flask, render_template
-from models import storage, State
-from flask import escape
+
+from models import storage
+from models.state import State
+
 
 app = Flask(__name__)
+'''The Flask application instance.'''
 app.url_map.strict_slashes = False
 
 
 @app.route('/states_list')
-def show_states():
-    '''Dictionary: of States'''
-    state_dict = storage.all(State)
-    return render_template('7-states_list.html', state_dict=state_dict)
+def states_list():
+    '''The states_list page.'''
+    all_states = list(storage.all(State).values())
+    all_states.sort(key=lambda x: x.name)
+    ctxt = {
+        'states': all_states
+    }
+    return render_template('7-states_list.html', **ctxt)
 
 
 @app.teardown_appcontext
-def teardown_db(self):
-    '''Deletes the current session'''
+def flask_teardown(exc):
+    '''The Flask app/request context end event listener.'''
     storage.close()
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
