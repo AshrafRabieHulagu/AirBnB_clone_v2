e1ca5efa7ad10b6c01af0844bab970ba031dc0a7
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-08-28 21:47:25 -0700

    0x03. AirBnB clone - Deploy static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,67 +1,54 @@
 #!/usr/bin/python3
-'''Module creates and distributes an archive to your web servers,
-using the function deploy:'''
+"""
+Fabric script based on the file 2-do_deploy_web_static.py that creates and
+distributes an archive to the web servers
 
+execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
+"""
+
+from fabric.api import env, local, put, run
 from datetime import datetime
-from fabric.api import local, env, put, sudo
-import os
-'''
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
-'''
-
-env.user = 'ubuntu'
+from os.path import exists, isdir
 env.hosts = ['18.214.88.86', '107.23.100.90']
 
 
-def do_deploy(archive_path):
-    '''Method to deploy archive'''
-
-    if os.path.exists(archive_path) is False:
-        return False
-
-    arch = archive_path.split("/", 2)[1]
-    new_file = '/data/web_static/releases/{}'.format(
-        arch.split(".", 1)[0])
-    tmp_file = '/tmp/{}'.format(arch)
-    cur = '/data/web_static/current'
-
-    '''
-    archive_path = versions/web_static_20210415160740.tgz
-    arch = web_static_20210415160740.tgz
-    new_file = /data/web_static/releases/web_static_20210415160740
-    tmp_file = /tmp/web_static_20210415160740.tgz
-    cur = /data/web_static/current
-    '''
-
-    try:
-        put(archive_path, '/tmp/')
-        sudo("mkdir -p {}/".format(new_file))
-        sudo("tar -xzf {} -C {}/".format(tmp_file, new_file))
-        sudo("rm {}".format(tmp_file))
-        sudo("mv {}/web_static/* {}".format(new_file, new_file))
-        sudo("rm -rf {}/web_static".format(new_file))
-        sudo("rm -rf {}".format(cur))
-        sudo("ln -s {}/ {}".format(new_file, cur))
-        print("YAY! LOVE YOU!")
-        return(True)
-    except BaseException:
-        print("KEEP TRYING! :)")
-        return(False)
-
-
 def do_pack():
-    '''Method to generate .tgz archive'''
-    local("mkdir -p versions")
-    timeStr = datetime.now().strftime('%Y%m%d%H%M%S')
-    tgzName = "versions/web_static_{}.tgz".format(timeStr)
-    local("tar -cvzf {} web_static".format(tgzName))
-    return (tgzName)
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
 
 
 def deploy():
-    '''Method that creates and distributes an archive to servers'''
-    newArc = do_pack()
-    if newArc is None:
-        return(False)
-    return(do_deploy(newArc))
+    """creates and distributes an archive to the web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
