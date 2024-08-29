b0bee66e23e1ea442c790287b134486275218bbf
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-08-29 01:48:11 -0700

    0x03. AirBnB clone - Deploy static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,52 +1,74 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers
+A Fabric script (based on the file 2-do_deploy_web_static.py) that creates
+and distributes an archive to your web servers, using the function deploy:
+
+    Prototype: def deploy():
+    The script should take the following steps:
+    Call the do_pack() function and store the path of the created archive
+    Return False if no archive has been created
+    Call the do_deploy(archive_path) function, using the new path of the new
+    archive
+    Return the return value of do_deploy
+    All remote commands must be executed on both of web your servers
+    (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
+    You must use this script to deploy it on your servers: xx-web-01 and
+    xx-web-02
 """
+from fabric.api import *
+from time import strftime
+import os.path
+env.hosts = ["18.214.88.86", "107.23.100.90"]
+env.user = "ubuntu"
 
-from fabric.api import env, local, put, run
-from datetime import datetime
-from os.path import exists, isdir
-env.hosts = ['18.214.88.86', '107.23.100.90']
 
-
+@runs_once
 def do_pack():
-    """generates a tgz archive"""
+    """
+    Fabric script that generates a .tgz archive from the contents of the
+    web_static folder of your AirBnB Clone repo.
+    """
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    present_time = strftime("%Y%M%d%H%M%S")
+    output = "versions/web_static_{}.tgz".format(present_time)
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except:
-        return None
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Distributes an archive to your web servers"""
+    if not os.path.isfile(archive_path):
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        filename = archive_path.split("/")[-1]
+        rmv_ext = filename.split(".")[0]
+        path_rmv_ext = "/data/web_static/releases/{}/".format(rmv_ext)
+        symlink = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        sudo("mkdir -p {}".format(path_rmv_ext))
+        sudo("tar -xzf /tmp/{} -C {}".format(filename, path_rmv_ext))
+        sudo("rm /tmp/{}".format(filename))
+        sudo("mv {}web_static/* {}".format(path_rmv_ext, path_rmv_ext))
+        sudo("rm -rf {}web_static".format(path_rmv_ext))
+        sudo("rm -rf {}".format(symlink))
+        sudo("ln -s {} {}".format(path_rmv_ext, symlink))
+        print("New version deployed!")
         return True
-    except:
+    except Exception:
         return False
 
 
 def deploy():
-    """creates and distributes an archive to the web servers"""
-    archive_path = do_pack()
-    if archive_path is None:
+    """creates and distributes an archive to your web servers"""
+    created_archive_path = do_pack()
+    if not created_archive_path:
         return False
-    return do_deploy(archive_path)
+    now_deploy = do_deploy(created_archive_path)
+    return now_deploy
