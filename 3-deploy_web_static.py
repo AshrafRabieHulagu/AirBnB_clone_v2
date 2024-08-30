3bab253bfe19676c2e82e99bce89b38afff77327
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-08-30 15:30:27 -0700

    0x03. AirBnB clone - Deploy static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Full deployment
 import os.path
 from datetime import datetime
 from fabric.api import env
@@ -7,11 +6,11 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["100.25.164.238", "54.89.108.249"]
+env.hosts = ['100.25.164.238', '54.89.108.249']
 
 
 def do_pack():
-    """do_pack"""
+    """Create a tar gzipped archive of the directory web_static."""
     dt = datetime.utcnow()
     file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
                                                          dt.month,
@@ -28,7 +27,14 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """do_deploy"""
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
     if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
@@ -62,7 +68,7 @@ def do_deploy(archive_path):
 
 
 def deploy():
-    """deploy"""
+    """Create and distribute an archive to a web server."""
     file = do_pack()
     if file is None:
         return False
